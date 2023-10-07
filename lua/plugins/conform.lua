bf4ba09625d76f9eaa793925d8b171d1c977a490
REMI NIVET <remi@Air-de-REMI.lan>
2023-10-07 13:23:52 +0200

    Add format keymap

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -18,8 +18,17 @@ return {
       },
       format_on_save = {
         lsp_fallback = true,
+        async = false,
         timeout_ms = 500,
       }
     })
+
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)", noremap = true, silent = true })
   end
 }
