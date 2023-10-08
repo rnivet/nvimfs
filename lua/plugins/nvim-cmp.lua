69ddb71ebdd6aa1d01968e35a56d3f9ec05a7631
REMI NIVET <remi@Air-de-REMI.lan>
2023-10-08 14:26:28 +0200

    Pimp completion

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -9,6 +9,7 @@ return {
     "rafamadriz/friendly-snippets",        -- useful snippets
     "onsails/lspkind.nvim",                -- vs-code like pictograms
     "hrsh7th/cmp-nvim-lsp-signature-help", -- source for method signatures
+    "hrsh7th/cmp-nvim-lua",                -- source form nvim lua
   },
   config = function()
     local cmp = require("cmp")
@@ -18,8 +19,15 @@ return {
     -- loads vscode style snippets from installed plugins (e.g. friendly-snippets)
     require("luasnip.loaders.from_vscode").lazy_load()
 
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
     cmp.setup({
       sources = cmp.config.sources({
+        { name = "nvim_lua" },
         { name = "nvim_lsp" },
         { name = "nvim_lsp_signature_help" },
         { name = "luasnip" }, -- snippets
@@ -42,14 +50,56 @@ return {
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
         ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
         ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        -- super tab mapping for snippets
+        ["<Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_next_item()
+            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+            -- they way you will only jump inside the snippet region
+          elseif luasnip.expand_or_jumpable() then
+            luasnip.expand_or_jump()
+          elseif has_words_before() then
+            cmp.complete()
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, { "i", "s" }),
       }),
       -- configure lspkind for vs-code like pictograms in completion menu
       formatting = {
         format = lspkind.cmp_format({
+          mode = "symbol_text",
           maxwidth = 50,
           ellipsis_char = "...",
+          menu = ({
+            buffer = "[Buf]",
+            nvim_lsp = "[Lsp]",
+            nvim_lsp_signature_help = "[Lsp]",
+            luasnip = "[Snip]",
+            nvim_lua = "[Lua]",
+            path = "[Path]",
+          })
         }),
-      }
+      },
+      view = {
+        entries = { name = 'custom', selection_order = 'near_cursor' }
+      },
+      window = {
+        documentation = cmp.config.window.bordered(),
+      },
+      experimental = {
+        ghost_text = true,
+        native_menu = false,
+      },
     })
   end
 }
