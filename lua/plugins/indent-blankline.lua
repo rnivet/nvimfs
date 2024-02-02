b2e1f41a13b43d0f18359447c3c16e99d931628d
Remi Nivet <remi.nivet@vadesecure.com>
2024-02-02 19:03:18 +0100

    Use pipe in indent even for tabs

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,6 +1,15 @@
 -- Show indent in margin plugin
 return {
   "lukas-reineke/indent-blankline.nvim",
+  event = { "BufReadPre", "BufNewFile" },
   main = "ibl",
-  opts = {}
+  config = function()
+    require("ibl").setup(
+      {
+        indent = {
+          tab_char = { "▎" },
+        }
+      }
+    )
+  end
 }
