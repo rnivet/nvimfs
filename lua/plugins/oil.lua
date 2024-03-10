8f8f274e7e39812ad5820d0cec6cdd2a29d3431d
Rémi Nivet <rnivet@gmail.com>
2024-03-10 17:39:46 +0100

    Add more Oil options

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -3,6 +3,15 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   config = function()
     require("oil").setup({
+      keymaps = {
+        ["s"] = "actions.select_vsplit",
+        ["q"] = "actions.close",
+      },
+      float = {
+        padding = 5,
+        max_width = 50,
+      },
     })
+    vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
   end
 }
